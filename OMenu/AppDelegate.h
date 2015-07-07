205e7af61c285b01576024f4bd5873c4b4c95f9d
Mustafa Onur Eken <oeken@Mustafas-MacBook-Air.local>
2015-07-07 11:53:54 +0300

    initial

---
+++ OMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OMenu
+//
+//  Created by Mustafa Onur Eken on 07/07/15.
+//  Copyright (c) 2015 Twentify. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
